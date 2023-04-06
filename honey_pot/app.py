970cf593575553e96ed7c36c372622a0ea53575f
garrett-vangilder <garrett.vangilder@gmail.com>
2023-04-06 08:09:56 -0500

    adds data parser

--- honey_pot/app.py
+++ honey_pot/app.py
@@ -1,11 +1,45 @@
-from flask import Flask
+import json
+from flask import Flask, request
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
-@app.route('/<string:web_path>')
-def honey_pot(web_path):
-    # TODO do something
-    return "Think, think, think."
+
+@app.route("/")
+@app.route("/<string:web_path>", methods=["GET", "POST", "PUT", "DELETE"])
+def honey_pot(web_path=""):
+    # serialize request information into byte stream using json
+    data = {}
+    try:
+        data = {
+            "headers": dict(request.headers),
+            "method": request.method,
+            "args": dict(request.args),
+            "form": dict(request.form),
+            "json": request.json,
+            "cookies": request.cookies,
+            "files": request.files,
+            "remote_addr": request.remote_addr,
+            "url": request.url,
+        }
+    except BadRequest:
+        data = {
+            "headers": dict(request.headers),
+            "method": request.method,
+            "args": dict(request.args),
+            "form": dict(request.form),
+            "json": {},
+            "cookies": request.cookies,
+            "files": request.files,
+            "remote_addr": request.remote_addr,
+            "url": request.url,
+        }
+
+    # Serialize the dictionary as JSON
+    json_data = json.dumps(data)
+
+    return json_data
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
